30074a06a2a7766732c42ad434347c63f32a79b9
Susanna Kosonen <susannakosonen@USF-Gold-Wifi-226-40-49.laptops.usf.edu>
2018-05-25 12:10:36 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  arr.sort_by! do |word|
+    word.chars.collect do |character|
+      esperanto.index(character)
+    end
+  end
+  arr
 end
